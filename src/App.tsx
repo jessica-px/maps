ee4cff88a80f1f23f675e2bac81c9df4926db7be
Jessica Peck <jessypeck@gmail.com>
2020-12-03 11:06:26 -0800

    Move ContentArea to own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,11 @@
-import React, { useState, useContext, ChangeEvent } from 'react';
+import React, { useContext } from 'react';
 import styled, { css } from 'styled-components'
-import ReactMarkdown from 'react-markdown'
 import { Map } from './Map';
-import { MapContextProvider, MapContext, getRoomById } from './MapContextProvider';
+import { ContentArea } from './ContentArea';
+import { MapContextProvider, MapContext } from './MapContextProvider';
 
 import "leaflet/dist/leaflet.css";
 
-// --------------------------------------------------------------- //
-//                           Custom Markdown                       //
-// --------------------------------------------------------------- //
-/*
-  Styling and rendering setup for customizing the markdown links:
-  now the [link](url) syntax expects the "url" to instead be a room
-  id. Clicking the link sets that room to active.
-  This doesn't really belong in this file and should be moved out.
-*/
-
-
-const MarkdownLink = styled.span`
-  font-weight: bold;
-  color: royalblue;
-  &:hover {
-    cursor: pointer;
-    text-decoration: underline;
-  }
-`
-
-const InternalLinkRenderer = ({href, children}: any) => {
-  const linkId = href;
-  const linkText = children[0].props.value;
-  const [state, dispatch] = useContext(MapContext);
-
-  const setActiveRoomId = (newId: string): void => {
-    dispatch({
-      type: 'Set_ACTIVE_ROOM_ID',
-      payload: newId
-    })
-  }
-
-  return (
-    <MarkdownLink onClick={() => setActiveRoomId(linkId)}>{linkText}</MarkdownLink>
-  );
-}
 
 // --------------------------------------------------------------- //
 //                       Styled Components                         //
@@ -98,105 +62,6 @@ const Sidebar = () => {
     </SidebarColumn>
   )
 }
-
-// --------------------------------------------------------------- //
-//                         Content Component                       //
-// --------------------------------------------------------------- //
-
-const ContentColumn = styled.div`
-  min-width: 400px;
-  flex: 2;
-  padding: 0 20px;
-  max-height: 100vh;
-  margin-bottom: 55px;
-  overflow: auto;
-  box-sizing: border-box;
-`
-
-const StyledTextArea = styled.textarea`
-  width: 96%;
-  height: 100%;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  padding: 5px;
-  resize: none;
-`
-
-const EditButton = styled.div`
-  &:hover {
-    color: royalblue;
-    cursor: pointer;
-    text-decoration: underline;
-  }
-`
-
-const EditMarkdownTextArea = () => {
-  const [ state, dispatch ] = useContext(MapContext);
-  const activeRoom = getRoomById(state.roomList, state.activeRoomId);
-
-  const updateRoomDescription = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    console.log('dispatch?')
-    console.log(state.activeRoomId)
-    dispatch({
-      type: 'UPDATE_ROOM_DESCRIPTION',
-      payload: {id: state.activeRoomId, description: event.target.value}
-    })
-    console.log(state.activeRoomId)
-  }
-
-  return (
-    <StyledTextArea
-      value={activeRoom.description}
-      onChange={updateRoomDescription}
-    />
-  )
-}
-
-const StyledMenu = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 5px 0;
-`
-
-interface MenuBarProps {
-  editModeEnabled: boolean,
-  setEditModeEnabled: (x: boolean) => void
-}
-
-const MenuBar = ({ editModeEnabled, setEditModeEnabled }: MenuBarProps) => {
-  return (
-    <StyledMenu>
-      <div>Menu Bar</div>
-      <EditButton
-        onClick={() => setEditModeEnabled(!editModeEnabled)}
-      >
-        {editModeEnabled ? 'Save' : 'Edit'}
-      </EditButton>
-    </StyledMenu>
-  )
-}
-
-const ContentArea = () => {
-  const [ editModeEnabled, setEditModeEnabled ] = useState(false);
-  const [ state, dispatch ] = useContext(MapContext);
-
-  const activeRoom = getRoomById(state.roomList, state.activeRoomId);
-
-  return (
-    <ContentColumn>
-      <MenuBar
-        editModeEnabled={editModeEnabled}
-        setEditModeEnabled={setEditModeEnabled}
-      />
-      <h1>{state.activeRoomId}. {activeRoom.name}</h1>
-      {editModeEnabled
-        ? <EditMarkdownTextArea />
-        : <ReactMarkdown renderers={{ "link":  InternalLinkRenderer }}>{activeRoom.description}</ReactMarkdown>
-      }
-    </ContentColumn>
-)}
 
 // --------------------------------------------------------------- //
 //                         Main Component                          //
