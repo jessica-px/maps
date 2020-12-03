a4320d9102fe2e430ecd622ac02024d2d650e405
Jessica Peck <jessypeck@gmail.com>
2020-12-03 09:42:53 -0800

    Tweak dummy data

--- src/MapContextProvider.tsx
+++ src/MapContextProvider.tsx
@@ -73,10 +73,10 @@ const roomList = [
     id: '3',
     name: `Officer's Quarters`,
     description: `## Description
-    \nA wooden table against the north wall is set with an earthenware jug of cider and a wooden cup. A wooden chair stands by the table. A bed stands against the west wall with a brass-bound, wooden chest against its foot. A lizardfolk warrior in scale armor sits on the bed, facing the door, as he sharpens his sword with a whetstone.
+    \nA wooden table against the north wall is set with an earthenware jug of cider and a wooden cup. A wooden chair stands by the table. A bed stands against the west wall with a brass-bound, wooden chest against its foot.
     \n## Creatures
-    \nThe officer (a **lizardfolk scaleshield**) leaps up, ready to fight, as soon as the characters enter.
-    \nIf the characters have avoided combat to this point, the lizardfolk in the barracks join the officer in this room when they hear sounds of combat.
+    \nAn officer (a **lizardfolk scaleshield**) sits on the bed, sharpening their sword. They leap up, ready to fight, as soon as the characters enter.
+    \nIf the characters have avoided combat to this point, the lizardfolk in the [Barracks](1) join the officer in this room when they hear sounds of combat.
     \n## Treasure
     \n The chest is locked and can be opened by a character who makes a successful DC 12 Dexterity check using thieves' tools or who has the key. It contains personal possessions, a purse containing 25 ep, a dagger in a scabbard, and a leather whip. The officer carries the key to the chest and he wears a silver collar (15 gp).
     \n## Exits
