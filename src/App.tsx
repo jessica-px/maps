909b4a2f654574a16363ae4fb18c98d4424eb5be
Jessica Peck <jessypeck@gmail.com>
2020-12-03 16:32:14 -0800

    Split Sidebar.tsx into own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
-import styled, { css } from 'styled-components';
+import React from 'react';
+import styled from 'styled-components';
 import { Map } from './mapPage/Map';
 import { ContentArea } from './mapPage/ContentArea';
-import { MapContextProvider, MapContext } from './mapPage/MapContextProvider';
+import { Sidebar } from './mapPage/Sidebar';
+import { MapContextProvider } from './mapPage/MapContextProvider';
 
 import 'leaflet/dist/leaflet.css';
 
@@ -15,73 +16,6 @@ const PageLayout = styled.div`
   flex-direction: row;
   height: 100vh;
 `;
-
-const SidebarColumn = styled.div`
-  width: 250px;
-  padding: 20px;
-  padding-left: 50px;
-`;
-
-interface SidebarItemProps {
-  active: boolean
-}
-
-const SidebarItem = styled.div<SidebarItemProps>`
-  padding: 10px 0;
-  &:hover {
-    font-weight: bold;
-    cursor: pointer;
-  }
-  ${(props) => props.active && css`
-    font-weight: bold;
-  `}
-`;
-
-const AddNewRoomButton = styled.div`
-  text-align: right;
-  &:hover {
-    color: royalblue;
-    cursor: pointer;
-    text-decoration: underline;
-  }
-`;
-
-// --------------------------------------------------------------- //
-//                         Sidebar Component                       //
-// --------------------------------------------------------------- //
-
-const Sidebar = () => {
-  const [state, dispatch] = useContext(MapContext);
-
-  const setActiveRoomId = (newId: string): void => {
-    dispatch({
-      type: 'Set_ACTIVE_ROOM_ID',
-      payload: newId
-    });
-  };
-
-  const addRoom = () => {
-    dispatch({
-      type: 'ADD_ROOM',
-      payload: null
-    });
-  };
-
-  return (
-    <SidebarColumn>
-      <AddNewRoomButton onClick={() => addRoom()}>+ Add Location</AddNewRoomButton>
-      {state.roomList.map((room, i) => (
-        <SidebarItem
-          key={room.id}
-          active={state.activeRoomId === room.id}
-          onClick={() => setActiveRoomId(room.id)}
-        >
-          {i + 1}. {room.name}
-        </SidebarItem>
-      ))}
-    </SidebarColumn>
-  );
-};
 
 // --------------------------------------------------------------- //
 //                         Main Component                          //
---
+++ src/mapPage/Sidebar.tsx
@@ -0,0 +1,77 @@
+import React, { useContext } from 'react';
+import styled, { css } from 'styled-components';
+import { MapContext } from '../mapPage/MapContextProvider';
+
+// --------------------------------------------------------------- //
+//                       Styled Components                         //
+// --------------------------------------------------------------- //
+
+// Container for the sidebar
+const SidebarColumn = styled.div`
+  width: 250px;
+  padding: 20px;
+  padding-left: 50px;
+`;
+
+interface SidebarItemProps {
+  active: boolean
+}
+
+// Items listed in the sidebar (room titles)
+const SidebarItem = styled.div<SidebarItemProps>`
+  padding: 10px 0;
+  &:hover {
+    font-weight: bold;
+    cursor: pointer;
+  }
+  ${(props) => props.active && css`
+    font-weight: bold;
+  `}
+`;
+
+// The "+ Add Location" button at the top of the sidebar
+const AddNewRoomButton = styled.div`
+  text-align: right;
+  &:hover {
+    color: royalblue;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
+
+// --------------------------------------------------------------- //
+//                         Main Component                          //
+// --------------------------------------------------------------- //
+
+export const Sidebar = () => {
+  const [state, dispatch] = useContext(MapContext);
+
+  const setActiveRoomId = (newId: string): void => {
+    dispatch({
+      type: 'Set_ACTIVE_ROOM_ID',
+      payload: newId
+    });
+  };
+
+  const addRoom = () => {
+    dispatch({
+      type: 'ADD_ROOM',
+      payload: null
+    });
+  };
+
+  return (
+    <SidebarColumn>
+      <AddNewRoomButton onClick={() => addRoom()}>+ Add Location</AddNewRoomButton>
+      {state.roomList.map((room, i) => (
+        <SidebarItem
+          key={room.id}
+          active={state.activeRoomId === room.id}
+          onClick={() => setActiveRoomId(room.id)}
+        >
+          {i + 1}. {room.name}
+        </SidebarItem>
+      ))}
+    </SidebarColumn>
+  );
+};
