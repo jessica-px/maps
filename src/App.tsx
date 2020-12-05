4c63cfbe8cf53a3d6f3b100a8859c9805e82e1c6
Jessica Peck <jessypeck@gmail.com>
2020-12-04 22:43:51 -0800

    Display directories and maps on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import {
   BrowserRouter as Router, Route, Switch, Link
@@ -7,7 +7,9 @@ import { Map } from './mapPage/Map';
 import { ContentArea } from './mapPage/ContentArea';
 import { Sidebar } from './mapPage/Sidebar';
 import { MapContextProvider } from './mapPage/MapContextProvider';
-import { UserContextProvider } from './UserContext';
+import {
+  UserContextProvider, UserContext, Directory, Map as MapType
+} from './UserContext';
 
 import 'leaflet/dist/leaflet.css';
 
@@ -57,12 +59,44 @@ const MapPage = () => (
   </MapContextProvider>
 );
 
-const HomePage = () => (
+const getMapById = (maps: MapType[], mapId: string) => {
+  for (const map of maps) {
+    if (map.id === mapId) {
+      return map;
+    }
+  }
+  throw new Error(`No map found with id of ${mapId}.`);
+};
+
+interface DirectoryContainerProps {
+  dir: Directory,
+  maps: MapType[]
+}
+
+const DirectoryContainer = ({ dir, maps }: DirectoryContainerProps) => (
   <>
-    <h1>Home Page</h1>
-    <Link to="/maps">Maps</Link>
+    <h3>{dir.name}</h3>
+    <ul>
+      {dir.mapIds.map((mapId) => {
+        const mapData = getMapById(maps, mapId);
+        return <li key={mapId}><Link to="/maps">{mapData.name}</Link></li>;
+      })}
+    </ul>
   </>
 );
+
+const HomePage = () => {
+  const [userState, dispatch] = useContext(UserContext);
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>Hello, {userState.name}</p>
+      {
+        userState.directories.map((dir) => <DirectoryContainer dir={dir} maps={userState.maps} key={dir.id} />)
+      }
+    </>
+  );
+};
 
 // --------------------------------------------------------------- //
 //                         Main Component                          //
